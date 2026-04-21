986164336e234cfffaad97f93e427404a4530997
w4149 <74163676+w4149@users.noreply.github.com>
2026-04-21 09:53:57 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const sentences = [
+  { id: 1, text: "找到你了。", x: 25, y: 25, audio: "audio/zdnl.mp3" },
+  { id: 2, text: "你还好吗？", x: 75, y: 25, audio: "audio/nhhm.mp3" },
+  { id: 3, text: "放松一下，好好休息。", x: 50, y: 40, audio: "audio/fsyyhhxx.mp3" },
+  { id: 4, text: "在画什么？", x: 25, y: 75, audio: "audio/zhsm.mp3" },
+  { id: 5, text: "我们一起......还是要爱！", x: 50, y: 60, audio: "audio/wmyqhsya.mp3" },
+  { id: 6, text: "你回来了。", x: 75, y: 75, audio: "audio/nhll.mp3" }
+];
